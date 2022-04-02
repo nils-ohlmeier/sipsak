e7bd8470fdc85d114d61085a3c943558b14c7f72
Nils Ohlmeier <github@ohlmeier.org>
2022-04-01 17:56:57 -0700

    replaced TIME_WITH_SYS_TIME with HAVE_SYS_TIME_H

--- src/shoot.c
+++ src/shoot.c
@@ -17,16 +17,10 @@
 
 #include "sipsak.h"
 
-#ifdef TIME_WITH_SYS_TIME
+#include <time.h>
+#ifdef HAVE_SYS_TIME_H
 # include <sys/time.h>
-# include <time.h>
-#else
-# ifdef HAVE_SYS_TIME_H
-#  include <sys/time.h>
-# else
-#  include <time.h>
-# endif
-#endif /* TIME_WITH_SYS_TIME */
+#endif
 #ifdef HAVE_UNISTD_H
 # ifdef HAVE_SYS_TYPES_H
 #  include <sys/types.h>
--- src/sipsak.c
+++ src/sipsak.c
@@ -2,7 +2,7 @@
  * Copyright (C) 2002-2004 Fhg Fokus
  * Copyright (C) 2004-2005 Nils Ohlmeier
  *
- * This file belongs to sipsak, a free sip testing tool.
+ * This file belongs to sipsak, a free SIP testing tool.
  *
  * sipsak is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -35,15 +35,9 @@
 # endif
 # include <unistd.h>
 #endif
-#ifdef TIME_WITH_SYS_TIME
+#include <time.h>
+#ifdef HAVE_SYS_TIME_H
 # include <sys/time.h>
-# include <time.h>
-#else
-# ifdef HAVE_SYS_TIME_H
-#  include <sys/time.h>
-# else
-#  include <time.h>
-# endif
 #endif
 #ifdef HAVE_SYS_WAIT_H
 # include <sys/wait.h>
